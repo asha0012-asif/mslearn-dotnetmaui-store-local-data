bcbdc9559e17d0bb47985eb2beebd37e9fa64006
Asif Ashadullah <156141923+asha0012-asif@users.noreply.github.com>
2025-07-19 20:43:28 -0400

    complete exercise part 1 - store data locally with SQLite

--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -1,4 +1,5 @@
 ﻿using People.Models;
+using SQLite;
 
 namespace People;
 
@@ -8,31 +9,34 @@ public class PersonRepository
 
     public string StatusMessage { get; set; }
 
-    // TODO: Add variable for the SQLite connection
+    private SQLiteConnection conn;
 
     private void Init()
     {
-        // TODO: Add code to initialize the repository         
+        if (conn != null)
+            return;
+
+        conn = new SQLiteConnection(_dbPath);
+        conn.CreateTable<Person>();
     }
 
     public PersonRepository(string dbPath)
     {
-        _dbPath = dbPath;                        
+        _dbPath = dbPath;
     }
 
     public void AddNewPerson(string name)
-    {            
+    {
         int result = 0;
         try
         {
-            // TODO: Call Init()
+            Init();
 
             // basic validation to ensure a name was entered
             if (string.IsNullOrEmpty(name))
                 throw new Exception("Valid name required");
 
-            // TODO: Insert the new person into the database
-            result = 0;
+            result = conn.Insert(new Person { Name = name });
 
             StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
         }
@@ -40,15 +44,14 @@ public class PersonRepository
         {
             StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
         }
-
     }
 
     public List<Person> GetAllPeople()
     {
-        // TODO: Init then retrieve a list of Person objects from the database into a list
         try
         {
-            
+            Init();
+            return conn.Table<Person>().ToList();
         }
         catch (Exception ex)
         {
